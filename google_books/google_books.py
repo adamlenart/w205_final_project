b5193ecf712fe6294846ff0f6382550378e7996c
agunapal <agunapal@ischool.berkeley.edu>
2016-11-28 16:02:16 -0800

    Google books

--- google_books/google_books.py
+++ google_books/google_books.py
@@ -4,6 +4,7 @@ api = googlebooks.Api()
 result = api.list('q:beautiful mind')
 
 for i in range(len(result["items"])):
-    if result["items"][i]["volumeInfo"].get("industryIdentifiers") is not None:
-        print result["items"][i]["volumeInfo"]["title"] ,result["items"][i]["volumeInfo"]["industryIdentifiers"][0]["identifier"]
+    if result["items"][i]["volumeInfo"].get("industryIdentifiers") is not None and result["items"][i]["volumeInfo"].get("categories") is not None:
+        print result["items"][i]["volumeInfo"]["title"] ,result["items"][i]["volumeInfo"]["industryIdentifiers"][0]["identifier"] ,\
+              result["items"][i]["volumeInfo"]["categories"][0]
 
