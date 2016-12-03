7be63aba83158132cc9d39aac6806a5e901c786d
agunapal <agunapal@ischool.berkeley.edu>
2016-12-02 22:01:14 -0800

    good reads

--- load_bigram/create_bigram_table.sql
+++ load_bigram/create_bigram_table.sql
@@ -5,4 +5,18 @@ match_count int,
 volume_count int)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
 STORED AS TEXTFILE
-LOCATION '/user/w205/literaly/bigram';
+LOCATION '/user/w205/literally/bigram';
+
+CREATE EXTERNAL TABLE IF NOT EXISTS good_reads
+(title string,
+isbn10 string,
+isbn13 string,
+rating int)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+"separatorChar" = ",",
+"quoteChar" = '"',
+"escapeChar" = '\\' )
+STORED AS TEXTFILE
+LOCATION '/user/w205/literally/good_reads';
+
