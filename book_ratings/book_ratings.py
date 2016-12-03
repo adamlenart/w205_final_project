259db4185feed8367046355d68c6fc9dd5bdc508
agunapal <agunapal@ischool.berkeley.edu>
2016-12-03 09:58:02 -0800

    good reads

--- book_ratings/book_ratings.py
+++ book_ratings/book_ratings.py
@@ -1,6 +1,17 @@
 from goodreads import client
-gc = client.GoodreadsClient(<api key>,<secret key>)
+import time
+import csv
+gc = client.GoodreadsClient("<key>", "<key>")
 
-book = gc.book(1)
-
-print book.title ,book.isbn, book.average_rating
+with open('book_reviews_1.csv','w') as csvfile:
+    bookwriter = csv.writer(csvfile, delimiter=',')
+                            #quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for i in range (49631,100000):
+        try:
+            book = gc.book(i)
+        except:
+            continue
+        time.sleep(1.5)
+        print i
+        if book.language_code == "eng":
+            bookwriter.writerow([book.title.encode('utf8') ,book.isbn, book.isbn13, book.average_rating])
