0882db38f9b0a4a3552dcc4d0fb8f95d786b59ad
Adam Lenart <alenart@ischool.berkeley.edu>
2016-12-07 16:13:03 +0100

    added dates

--- google_books/google_books_no_rating.py
+++ google_books/google_books_no_rating.py
@@ -8,38 +8,54 @@ import numpy as np
 # as Google limits the API results to 40, we need to call the GoogleBooksQuery multiple times to get all of the results.
 # The get_all_books() function should do that.
 class GoogleBooksQuery():
-    
+    '''A Google Books Query queries the Google Books API '''
     # count number of instances
     count = 0
     
     # as there is an upper limit of daily requests sent to the Google API, rather than wasting an extra call just to get
     # the number of total results, let the class already return the results and keep track how many times it was called
     def __init__(self,phrase,maxResults=40):
+        '''A GoogleBooksQuery has a url request, the json result of the request and the number of times the query was
+           instantiated.
+         
+        Parameters:
+        -----------
+        phrase: string, the queried expression on Google Books
+        maxResults: int between 1 and 40, the maximum number of results the query should return.
+        '''
+
         self.r = requests.get(url="https://www.googleapis.com/books/v1/volumes", params={"q":phrase,
                                         "maxResults":maxResults,"startIndex":GoogleBooksQuery.count*maxResults})
         self.result = self.r.json()
         GoogleBooksQuery.count += 1
     
-    @property
-    def totalItems(self):
-        return self.result["totalItems"]
-    
-
-        
+      
     def extract_information(self):
-        
-        result = pd.DataFrame(columns=('title', 'ISBN', 'category','rating','date'))
+        '''Get title, ISBN, category and date for a Google Books query.'''
+
+        result = pd.DataFrame(columns=('title', 'ISBN', 'category','date'))
         item_counter = 0
         for item in self.result["items"]:
             volume_info = item["volumeInfo"]
             if volume_info.get("industryIdentifiers") is not None and volume_info.get("categories") is not None and \
-                volume_info.get("publishedDate") is not None and volume_info.get("averageRating") is not None:
+                volume_info.get("publishedDate") is not None :
                 result.loc[item_counter] = [volume_info["title"] ,volume_info["industryIdentifiers"][0]["identifier"],\
-                volume_info["categories"][0],volume_info["averageRating"],volume_info["publishedDate"]]
+                volume_info["categories"][0],volume_info["publishedDate"]]
                 item_counter += 1
         return result
                 
-def get_all_books(phrase,mid_year=True):
+def get_all_books(phrase,mid_date=True):
+    '''Returns all of the results of a Google Books query.  Deletes records with non-conforming date-types from (e.g.,
+    198? instead of 1983), adds decades and years.
+    Parameters:
+    -----------
+    phrase: query Google Books for phrase
+    mid_date: boolean, if true, converts the records with only a known publication year to "year-07-01", or if month is known than "year-month-15"
+    Returns:
+    --------
+    A pandas dataframe containing title, ISBN, category and date as columns and books as rows
+    '''
+
     googlebook = GoogleBooksQuery(phrase)
     books = googlebook.extract_information()
     books['year'] = books['date']
@@ -55,7 +71,7 @@ def get_all_books(phrase,mid_year=True):
         try:
             books['year'][i] = re.search(r'\d{4}',date).group()
             # substitute mid-year if publication date is not known to day
-            if mid_year is True and re.search(r'\d{4}-\d{2}-\d{2}',date) is None:
+            if mid_date is True and re.search(r'\d{4}-\d{2}-\d{2}',date) is None:
                 if re.search(r'\d{4}-\d{2}',date) is None:
                     books['date'][i] = date+"-07-01"
                 else:
@@ -66,9 +82,9 @@ def get_all_books(phrase,mid_year=True):
     if len(wrong_year)>0:
         books = books.drop(books.index[wrong_year])
     # calculate decades
-    books['decade'] = np.floor_divide(map(int,books['year']),10)*10        
+    books['decade'] = np.floor_divide(map(int,books['year']),10)*10 
+    books['datetime'] = books['date']+" 12:00:00"      
     return books
-
 
 
 # execute from command line
@@ -76,3 +92,4 @@ if __name__ == '__main__':
     result = get_all_books(sys.argv[1])
     result.to_csv(sys.argv[2],sep="\t",encoding="utf-8",header=False,index=False)
     print result
+
--- google_books/google_books_pyspark_expanded.py
+++ google_books/google_books_pyspark_expanded.py
@@ -1,12 +1,15 @@
-import re
 import sys
 import os
 import requests
 import pandas as pd
 import numpy as np
+from pyspark.sql import HiveContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from pyspark import SparkContext
+from pyspark import HiveContext
 
-# as Google limits the API results to 40, we need to call the GoogleBooksQuery multiple times to get all of the results.
-# The get_all_books() function should do that.
+
 class GoogleBooksQuery():
     
     # count number of instances
@@ -69,10 +72,46 @@ def get_all_books(phrase,mid_year=True):
     books['decade'] = np.floor_divide(map(int,books['year']),10)*10        
     return books
 
+def create_google_books_table(hive_context, book_list):
+    '''Function used to create a SQL table called google_books based on the query phrase
+       We store the title, isbn13, category and rating'''
 
+    schema = StructType([
+        StructField("isbn13", StringType(), True),
+        StructField("Category", StringType(), True),
+        StructField("Date", StringType(), True),
+        StructField("Rating", StringType(), True),
+        StructField("Title", StringType(), True),
+        StructField("Year", StringType(), True),
+        StructField("Decade", StringType(), True),
+    ])
+
+    DF = hive_context.createDataFrame(book_list,schema)
+    DF.registerTempTable('google_books_temp')
+    results = hive_context.sql('SELECT * FROM google_books_temp limit 10')
+    hive_context.sql("DROP TABLE IF EXISTS google_books")
+    hive_context.sql("CREATE TABLE google_books AS SELECT * FROM google_books_temp")
+    return 
+
+def merge_google_books_good_reads(hive_context):
+    """Merge the good_reads table and google_books table with the common key as isbn13.
+       We average the ratings from the 2 tables"""
+ 
+    good_reads = hive_context.table("default.good_reads")
+    good_reads.registerTempTable("good_reads_temp")
+    #hive_context.sql("select * from good_reads_temp").show()
+    hive_context.sql("DROP TABLE IF EXISTS query_bigram_result")
+    hive_context.sql('CREATE TABLE query_bigram_result AS SELECT google_books.title, google_books.isbn13, google_books.category, google_books.date, google_book.year, google_books.decade, AVG(CASE WHEN good_reads_temp.rating > 0 THEN (google_books.rating + good_reads_temp.rating)/2 ELSE google_books.rating END) as rating FROM google_books LEFT JOIN good_reads_temp ON google_books.isbn13 = good_reads_temp.isbn13 GROUP BY google_books.title,google_books.isbn13, google_books.category')
+    merged_table = hive_context.sql('SELECT * from query_bigram_result')
+    merged_table.show()
+    return
 
 # execute from command line
 if __name__ == '__main__':
-    result = get_all_books(sys.argv[1])
-    result.to_csv(sys.argv[2],sep="\t",encoding="utf-8",header=False,index=False)
-    print result
+    book_list = get_all_books(sys.argv[1])
+    sc = SparkContext()
+    sqlContext = SQLContext(sc)
+    hive_context = HiveContext(sc)
+    create_google_books_table(hive_context,book_list) 
+
+    merge_google_books_good_reads(hive_context)
