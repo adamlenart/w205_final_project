1e5f806c970897c2bdffaa84bb53943ff4a0e7e4
Adam Lenart <alenart@ischool.berkeley.edu>
2016-12-07 03:41:13 +0100

    Added dates

--- google_books/google_books.py
+++ google_books/google_books.py
@@ -1,7 +1,9 @@
+import re
 import sys
 import os
 import requests
 import pandas as pd
+import numpy as np
 
 # as Google limits the API results to 40, we need to call the GoogleBooksQuery multiple times to get all of the results.
 # The get_all_books() function should do that.
@@ -29,31 +31,37 @@ class GoogleBooksQuery():
     
       
     def extract_information(self):
-        '''Get title, ISBN and category for a Google Books query.'''
+        '''Get title, ISBN, category and date for a Google Books query.'''
 
-        result = pd.DataFrame(columns=('title', 'ISBN', 'category'))
+        result = pd.DataFrame(columns=('title', 'ISBN', 'category','date'))
         item_counter = 0
         for item in self.result["items"]:
             volume_info = item["volumeInfo"]
-            if volume_info.get("industryIdentifiers") is not None and volume_info.get("categories") is not None:
+            if volume_info.get("industryIdentifiers") is not None and volume_info.get("categories") is not None and \
+                volume_info.get("publishedDate") is not None :
                 result.loc[item_counter] = [volume_info["title"] ,volume_info["industryIdentifiers"][0]["identifier"],\
-                volume_info["categories"][0]]
+                volume_info["categories"][0],volume_info["publishedDate"]]
                 item_counter += 1
         return result
                 
-def get_all_books(phrase):
-    '''Returns all of the results of a Google Books query.
+def get_all_books(phrase,mid_date=True):
+    '''Returns all of the results of a Google Books query.  Deletes records with non-conforming date-types from (e.g.,
+    198? instead of 1983), adds decades and years.
 
     Parameters:
     -----------
     phrase: query Google Books for phrase
+    mid_date: boolean, if true, converts the records with only a known publication year to "year-07-01", or if month is known than "year-month-15"
 
     Returns:
     --------
-    A pandas dataframe containing title, ISBN and category as columns and books as rows
+    A pandas dataframe containing title, ISBN, category and date as columns and books as rows
     '''
+
     googlebook = GoogleBooksQuery(phrase)
     books = googlebook.extract_information()
+    books['year'] = books['date']
+    wrong_year = []
     while True:
         try:
             googlebook = GoogleBooksQuery(phrase)
@@ -61,6 +69,23 @@ def get_all_books(phrase):
             books = books.append(new_books,ignore_index=True)
         except KeyError as e:
             break
+    for i,date in enumerate(books['date']):
+        try:
+            books['year'][i] = re.search(r'\d{4}',date).group()
+            # substitute mid-year if publication date is not known to day
+            if mid_date is True and re.search(r'\d{4}-\d{2}-\d{2}',date) is None:
+                if re.search(r'\d{4}-\d{2}',date) is None:
+                    books['date'][i] = date+"-07-01"
+                else:
+                    books['date'][i] = date+"-15"
+        except AttributeError as er:
+            wrong_year.append(i)
+    # remove years in a wrong format
+    if len(wrong_year)>0:
+        books = books.drop(books.index[wrong_year])
+    # calculate decades
+    books['decade'] = np.floor_divide(map(int,books['year']),10)*10 
+    books['datetime'] = books['date']+" 12:00:00"      
     return books
 
 
