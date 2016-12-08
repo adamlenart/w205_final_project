4397258423c4f668f04ed7377ea1a7217fb72760
Adam Lenart <alenart@ischool.berkeley.edu>
2016-12-08 11:59:35 +0100

    added ratings to visualizations

--- visualizations/server.r
+++ visualizations/server.r
@@ -1,11 +1,17 @@
 # server.R
 library(networkD3)
 library(treemap)
+library(plotly)
 
+# define auxiliary functions
+
+# functions for downloading data
 source("/home/adam/Documents/MIDS/W205/project/shiny-literally/import_data.r")
 
-get_links <- function(keep_n_cats,data) {
-  foo <- transform_data(keep_n_cats,data)
+
+# build network data
+get_links <- function(keep_n_cats,data,source="source",target="target",value="value") {
+  foo <- transform_data(keep_n_cats,data, value=value)
   nodes <- data.frame(name=unique(c(foo$source,foo$target)))
   links<- data.frame(source=match(foo$source,nodes$name)-1,
                      target=match(foo$target,nodes$name)-1,
@@ -23,17 +29,28 @@ get_nodes <- function(keep_n_cats,data) {
 }
 
 
-transform_data <- function(keep_n_cats,network_data) {
-if(is.na(keep_n_cats)==FALSE) {
-  frequent_categories <- network_data %>% group_by(source) %>% summarize(total=sum(value)) %>% top_n(keep_n_cats)
-  network_data <- network_data %>% inner_join(frequent_categories)
-}
+transform_data <- function(keep_n_cats,data,value="value") {
+  if(value=="value") {
+     if(is.na(keep_n_cats)==FALSE) {
+        frequent_categories <- data %>% group_by(source) %>% summarize(total=sum(value)) %>% top_n(keep_n_cats)
+        network_data <- data %>% inner_join(frequent_categories)
+     }
+  }
+  if(value=="rating") {
+    if(is.na(keep_n_cats)==FALSE) {
+      frequent_categories <- data %>% group_by(category) %>% summarize(total=n()) %>% top_n(keep_n_cats)
+      network_data <- data %>% inner_join(frequent_categories)
+    } 
+  }  
   return(network_data)
 }
 
+# get data
+db_data <- get_data()
+network_data = db_data[[1]]
+rating_data = db_data[[2]] # there are fewer ratings than books, so it is better to keep them separately
 
-network_data = get_data()
-
+# define server side process
 shinyServer(
   function(input, output, session) {
    
@@ -50,6 +67,11 @@ shinyServer(
   	     treemap(transform_data(input$filter2,network_data),fontsize.labels=input$font2,
   	             index=c("source","target"),vSize="value",title="") 
   	 
+  	) # close the treemap
+  	
+  	output$boxplot <- renderPlotly(
+  	  plot_ly(transform_data(input$filter3,rating_data,value="rating"), y=~rating, x=~category,type="box")   
+  	    
   	)
 
 
